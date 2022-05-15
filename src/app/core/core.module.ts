5e95b258930c6c1ec8ceb1915ddc6885b2a539fc
syuancao <73942181+syuancao@users.noreply.github.com>
2022-05-15 10:13:56 +0800

    add sidebar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,13 @@ import { NgModule, SkipSelf, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarComponent
   ],
   exports: [
     HeaderComponent,
